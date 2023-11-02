91e9a4275a479a870d5eac9f2af91226d1c11853
Valeriia Lyakisheva <vorik07@icloud.com>
2023-11-02 11:07:36 +0100

    Router installed and modules reinstalled

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/spa-react-todo-list-LyaLera/"
+  plugins: [react()]
 })
